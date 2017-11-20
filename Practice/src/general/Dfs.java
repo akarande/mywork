7bc0c5f6814afe1fa527ad0357fae665e731e229
Ajinkya Karande <akarande@salesforce.com>
2017-11-19 23:35:54 -0800

    added dfs

---
+++ Practice/src/general/Dfs.java
@@ -0,0 +1,41 @@
+package general;
+
+import java.util.Arrays;
+
+public class Dfs {
+
+	boolean visited[];
+	int g[][];
+	
+	void init() {
+		 g = new int[][] {{0, 1, 0, 1, 0, 0, 0, 0, 1},
+						  {1, 0, 0, 0, 0, 0, 0, 1, 0},
+						  {0, 0, 0, 1, 0, 1, 0, 1, 0},
+						  {1, 0, 1, 0, 1, 0, 0, 0, 0},
+						  {0, 0, 0, 1, 0, 0, 0, 0, 1},
+						  {0, 0, 1, 0, 0, 0, 1, 0, 0},
+						  {0, 0, 0, 0, 0, 1, 0, 0, 0},
+						  {0, 1, 1, 0, 0, 0, 0, 0, 0},
+						  {1, 0, 0, 0, 1, 0, 0, 0, 0}
+				};
+		visited = new boolean[g.length];
+		Arrays.fill(visited, false);
+	}
+	
+	public void dfs(int i) {
+		visited[i] = true;
+		System.out.print(i + "\t");
+		for(int j = 0; j < g.length; j++) {
+			if(!visited[j] && g[i][j] == 1) {
+				dfs(j);
+			}
+		}
+	}
+	
+	public static void main(String arg[]) {
+		
+		Dfs dfs = new Dfs();
+		dfs.init();
+		dfs.dfs(0);
+	}
+}
