83fb3cc2999bc223c97efbe9fdb64397911d83c2
Ajinkya Karande <akarande@salesforce.com>
2017-11-26 13:44:01 -0800

    longest palindromic subsequence

---
+++ Practice/src/leetcode/LongestPalindromicSubsequence.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+public class LongestPalindromicSubsequence {
+	
+	/**
+	 * Fill bottom up for easier operations
+	 * @param s
+	 * @return
+	 */
+	public int longestPlaindromicSubsequence(String s) {
+		int n = s.length();
+		int dp[][] = new int[n][n];
+		for(int i = n-1; i >= 0; i--) {
+			dp[i][i] = 1;
+			for(int j = i+1; j < n; j++) {
+				if(s.charAt(i) == s.charAt(j)) dp[i][j] = 2 + dp[i+1][j-1];
+				else dp[i][j] = Math.max(dp[i+1][j], dp[i][j-1]);
+			}
+		}
+		return dp[0][n-1];
+	}
+	
+	public static void main(String arg[]) {
+		String s = "agbdba";
+		LongestPalindromicSubsequence lps = new LongestPalindromicSubsequence();
+		System.out.println(lps.longestPlaindromicSubsequence(s));
+	}
+}
